8a185f693f2a2fe06ee3d96e70625680eb0a11b0
Thaís Campos <89364138+T1008@users.noreply.github.com>
2022-05-02 10:33:22 -0300

    Projeto Alterado

--- projeto1.py
+++ projeto1.py
@@ -3,3 +3,9 @@ parcelas = int(input('Insira o número de parcelas: '))
 
 if (parcelas <= 0):
     print('Seu pagamento será a vista')
+    
+elif (parcelas <= 3): 
+    X1 = produto*0.1;
+    x2 = produto + X1;
+    x3 = x2/parcelas
+    print("Print o valor do seu produto é igual à:",x2,"\n o valor da parcela:",x3)
